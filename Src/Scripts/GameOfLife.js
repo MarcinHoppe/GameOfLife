4f423eea7398d9653267cdd4ad531c6d6ef57b06
Marcin Hoppe <marcin.hoppe@gmail.com>
2014-05-06 14:09:56 +0200

    Implement rules

--- Src/Scripts/GameOfLife.js
+++ Src/Scripts/GameOfLife.js
@@ -9,23 +9,83 @@ function LifeViewModel() {
 
     self.start = function () {
         self.shouldAllowStart(false);
-        setInterval(self.game.makeStep, 100);
+        setInterval(self.game.makeStep, 500);
     };
 }
 
 function Game(size) {
-    this.rows = makeRows(size);
+    var self = this;
+
+    self.rows = makeRows(size);
+    this.makeStep = function () {
+        var oldGrid = scan(self.rows),
+            newGrid = update(oldGrid);
+        render(self.rows, newGrid);
+    }
 }
 
-Game.prototype.makeStep = function () {
-    
+function scan(rows) {
+    var grid = [],
+        i, j;
+    for (i = 0; i < rows.length; ++i) {
+        grid.push([]);
+        for (j = 0; j < rows.length; ++j) {
+            grid[i].push(rows[i].cells[j].valueOf());
+        }
+    }
+    return grid;
+}
+
+function update(grid) {
+    var result = [],
+        i, j;
+    for (i = 0; i < grid.length; ++i) {
+        result.push([]);
+        for (j = 0; j < grid.length; ++j) {
+            result[i].push(0);
+            var neighbours = [
+                { row: i - 1, column: j - 1 },
+                { row: i - 1, column: j     },
+                { row: i - 1, column: j + 1 },
+                { row: i    , column: j - 1 },
+                { row: i    , column: j     },
+                { row: i    , column: j + 1 },
+                { row: i + 1, column: j - 1 },
+                { row: i + 1, column: j     },
+                { row: i + 1, column: j + 1 }
+            ];
+            neighbours.forEach(function (cell) {
+                if (grid[cell.row] !== undefined && grid[cell.row][cell.column] !== undefined) {
+                    result[i][j] += grid[cell.row][cell.column];
+                }
+            });
+        }
+    }
+    return result;
+}
+
+function render(rows, grid) {
+    var i, j;
+    for (i = 0; i < rows.length; ++i) {
+        for (j = 0; j < rows.length; ++j) {
+            if (grid[i][j] === 3) {
+                rows[i].cells[j].isAlive(true);
+            } else if (grid[i][j] === 4) {
+                // Retain state.
+            } else {
+                rows[i].cells[j].isAlive(false);
+            }
+        }
+    }
 }
 
 function makeRows(howMany) {
     var result = [],
         i;
     for (i = 0; i < howMany; ++i) {
-        result.push({ cells: makeCells(howMany) });
+        result.push({
+            cells: makeCells(howMany)
+        });
     }
     return result;
 }
@@ -43,6 +103,9 @@ function Cell() {
     this.isAlive = ko.observable(false);
     this.toggle = function () {
         this.isAlive(!this.isAlive());
+    };
+    this.valueOf = function () {
+        return this.isAlive() ? 1 : 0;
     };
 }
 
